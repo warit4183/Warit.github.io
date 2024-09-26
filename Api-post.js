bf849274699884862434d5006dcef9d134ee9651
warit4183 <Waritsuktornz@gmail.com>
2024-09-26 15:53:07 +0700

    Maybe final

---
+++ Api-post.js
@@ -0,0 +1,29 @@
+async function submitData() {
+    const studentId = document.getElementById("studentId").value;
+    const studentName = document.getElementById("studentName").value;
+
+    // สร้างข้อมูลในรูปแบบ JSON
+    const data = {
+        id: studentId,
+        name: studentName
+    };
+
+    try {
+        const response = await fetch("http://206.189.146.138/api/students", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            alert("ส่งสำเร็จ");
+        } else {
+            alert("ส่งไม่สำเร็จ!");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        alert("ส่งไม่สำเร็จ!");
+    }
+}
---
+++ Api-sensor.js
@@ -0,0 +1,13 @@
+function updateData() {
+    fetch('http://206.189.146.138/api/sensors')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('timestamp').textContent = data.timestamp;
+        document.getElementById('temperature').textContent = data.temperature.toFixed(2);
+        document.getElementById('luxsensor').textContent = data.luxsensor.toFixed(2);
+        document.getElementById('humidity').textContent = data.humidity.toFixed(2);
+        document.getElementById('motion').textContent = data.motion;
+    });
+}
+
+setInterval(updateData, 1000); 
